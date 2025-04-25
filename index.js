52c218eac9cb9f2ff419f5e5e333b4ef6b014565
muhamsh <muhammad-ashraful@outlook.com>
2025-04-26 01:46:49 +0600

    async error handle

--- index.js
+++ index.js
@@ -1,40 +1,103 @@
 const express = require( 'express' );
 const app = express();
+const fs = require( 'fs' );
 const port = process.env.PORT || 5000;
 
-app.get( '/', ( req, res ) =>
-{
-    // res.send( 'Hello Express.js!' );
-    // res.send(a)
-    for ( let i = 0; i < 10; i++ )
-    {
-        if(i === 9)
-        {
-           next( new Error( 'This is an error! get route --> for loop' ) );
-        }
-        else
-        {
-            res.write('Hello Express.js! ' + i + '\n');
-        }
-    }
-
-    res.end();
-} );
-
-// app.get( '/api', ( req, res ) =>
+// app.get( '/', ( req, res ) =>
 // {
-//     // throw new Error( 'This is an error!' );
+//     // res.send( 'Hello Express.js!' );
 //     // res.send(a)
-// })
-app.use( ( req, res, next ) =>
-{
-    // console.log( 'Middleware 1' );
-    // next();
-    // throw new Error( 'This is an error!' );
-    // next( new Error( 'This is an error!' ) );
-    // res.status(404).send( '404 URL Not Found' );
-    next( 'bhai error hoise' );
-} );
+//     for ( let i = 0; i < 10; i++ )
+//     {
+//         if(i === 9)
+//         {
+//            next( new Error( 'This is an error! get route --> for loop' ) );
+//         }
+//         else
+//         {
+//             res.write('Hello Express.js! ' + i + '\n');
+//         }
+//     }
+
+//     res.end();
+// } );
+
+// // app.get( '/api', ( req, res ) =>
+// // {
+// //     // throw new Error( 'This is an error!' );
+// //     // res.send(a)
+// // })
+// app.use( ( req, res, next ) =>
+// {
+//     // console.log( 'Middleware 1' );
+//     // next();
+//     // throw new Error( 'This is an error!' );
+//     // next( new Error( 'This is an error!' ) );
+//     // res.status(404).send( '404 URL Not Found' );
+//     next( 'bhai error hoise' );
+// } );
+
+// app.use( ( error, req, res, next ) =>
+// {
+//     // console.log( error );
+//     // res.status( 500 ).send( 'bhai error hoise' );
+//     if ( res.headersSent )
+//     {
+//         next( 'problem in headers' );
+//     } else
+//     {
+//         if ( error && error.message )
+//         {
+//             res.send( error.message );
+//         }
+//         else
+//         {
+//             res.send( 'bhai error hoise' );
+//         }
+//     }
+// } );
+
+
+// app.get( '/', ( req, res, next ) =>
+// {
+//     fs.readFileSync( './index.html', ( error, data ) =>
+//     {
+//         if ( error )
+//         {
+//             next( error );
+//         }
+//         else
+//         {
+//             res.send( data );
+//         }
+//     })
+//     // setTimeout( () =>
+//     // {
+//     //     try
+//     //     {
+//     //         res.send( e );
+//     //     } catch ( error )
+//     //     {
+//     //         next( error );
+//     //     }
+//     // }, 100 );
+// } );
+
+app.get( '/', [
+    ( req, res, next ) =>
+    {
+        fs.readFileSync( './index.html', ( error, data ) =>
+        {
+            console.log( data )
+            next();
+        } );
+    },
+    ( req, res, next ) =>
+    {
+        console.log( data.property )
+        // next();
+    },
+] );
 
 app.use( ( error, req, res, next ) =>
 {
@@ -47,7 +110,7 @@ app.use( ( error, req, res, next ) =>
     {
         if ( error && error.message )
         {
-            res.send( error.message );
+            res.status(500).send( error.message );
         }
         else
         {
